82f0df4e4ed8fbfb4c52e74dd08c24a95b5fbc99
pedro.lima350 <pedro.lima350@etec.sp.gov.br>
2025-04-22 20:03:28 -0300

    Estrutura base

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Op = "";
+            while (Op != "q")
+            {
+                Console.WriteLine("teste, [Digite q se quiser sair]");
+                Op = Console.ReadLine();
+                Console.WriteLine("teste 2 ");
+                Op = Console.ReadLine();
+
+            }
+
+
+        }
+    }
+}
